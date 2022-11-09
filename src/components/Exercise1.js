faff87b1e87faa74aa873466bc78debbf55fee76
edengil <edengil94@gmail.com>
2022-11-09 12:06:42 +0200

    Exercise 1

--- src/components/Exercise1.js
+++ src/components/Exercise1.js
@@ -9,13 +9,22 @@ class Exercise1 extends Component {
             age: ""
         }
     }
-
+    addText = (event) =>{
+        const  target = event.target
+        const val = target.id === "name-input" ? "name" : "age"
+        this.setState({
+            [val]: target.value
+        })
+    }
+    alertText = () => alert(`${this.state.name} ${this.state.age}`)
     render() {
         return (
             <div>
-                <input id="name-input" />
-                <input id="age-input" />
-                <button>Go to Bar</button>
+                <input id="name-input"
+                 value={this.state.name} onChange={this.addText}/>
+                <input id="age-input"
+                 value={this.state.age} onChange={this.addText}/>
+                <button onClick={this.alertText}>Go to Bar</button>
             </div>
         );
     }
