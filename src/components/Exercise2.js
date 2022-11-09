63c4357335cf9f9222f7087fb7d6f9fba1b00d1b
edengil <edengil94@gmail.com>
2022-11-09 12:23:13 +0200

    the end

--- src/components/Exercise2.js
+++ src/components/Exercise2.js
@@ -9,12 +9,35 @@ constructor() {
         fruit: ""
     }
 }
+ handleChange = event => {
+    this.setState({
+        fruit:event.target.value
+    })
+};
+addText = (event) =>{
+    const  target = event.target
+    this.setState({
+        name: target.value
+    }, () => {
+        console.log(this.state.name,this.state.fruit);
+    }
+    )
+}
 
 render() {
     return (
         <div>
-            <input id="name-input" />
-            <select id="select-input"></select>
+            <input 
+            id="name-input" 
+            value={this.state.name}
+            onChange={this.addText} />
+            <select onChange={this.handleChange} id="select-input">
+                <option value="volvo"></option>
+                <option value="volvo">Volvo</option>
+                <option value="saab">Saab</option>
+                <option value="mercedes">Mercedes</option>
+                <option value="audi">Audi</option>
+            </select>
         </div>
     );
 }
